34452f247400ba99ef26d9a80e4c0be70e1d5e83
Ming Wei Liu <n000165242@outlook.com>
2025-03-16 19:49:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzxD78A0WllYWS8RSg9Ac5hAijxwn9wSvezHxQtJ5BB3JAX9B2yK8GWu3cmPxxSVixX/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxHpvf2K0GUoDLmFP1nKBR9efrJl0PUC6XCB8_H--DS3I4_48OVNwQu9yw_S46RGBE/exec";
 
 let allUsers = [];
 let custodiansData = {}; // 存放每個物品的保管人清單
